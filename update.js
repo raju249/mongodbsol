d3f4a106d08696f3efad2f1b94827d947b18da73
Rajendra kadam <rajendrakadam249@gmail.com>
2015-11-16 06:41:34 +0000

    Updating data in mongodb

---
+++ update.js
@@ -0,0 +1,19 @@
+
+var db = process.argv[2];
+var url = "mongodb://localhost:27017/"+db;
+var mongo = require('mongodb').MongoClient
+
+mongo.connect(url,function(err,db){
+    if (err) throw err
+    var users = db.collection('users');
+    users.update({
+        username:"tinatime"
+    },{
+        $set:{
+            age:40
+        }
+    },function(err,data){
+        if (err) throw err
+        db.close()
+    })
+})

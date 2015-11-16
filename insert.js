21a81c65a47bcbf5f1d8b2edea54f0975912edba
Rajendra kadam <rajendrakadam249@gmail.com>
2015-11-16 06:04:29 +0000

    Inserting in mongodb

---
+++ insert.js
@@ -0,0 +1,22 @@
+var url = "mongodb://localhost:27017/learnyoumongo"
+var mongo = require('mongodb').MongoClient
+
+var firstName = process.argv[2];
+var lastName = process.argv[3];
+
+var doc = {
+  firstName: firstName
+, lastName: lastName
+}
+mongo.connect(url, function(err, db) {
+      if (err) throw err
+      
+      var users = db.collection('users');
+      
+      users.insert(
+          doc,function(err,data){
+          if (err) throw err
+          console.log(JSON.stringify(doc))
+          db.close()
+      })
+    })
